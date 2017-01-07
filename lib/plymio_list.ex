4372ef6774f8230bada472bb9fe3217cc8179b97
Ian Rumford <ian@taniwha>
2017-01-07 19:32:19 +0000

    plymio_list: Elixir Utility Functions for Lists

---
+++ lib/plymio_list.ex
@@ -0,0 +1,18 @@
+defmodule PlymioList do
+  @moduledoc """
+  Documentation for PlymioList.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlymioList.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plymio_list_test.exs
@@ -0,0 +1,8 @@
+defmodule PlymioListTest do
+  use ExUnit.Case
+  doctest PlymioList
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
